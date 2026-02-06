c13a36e3d2b9a0e415e49ba952e73825b6d74efa
Muhammad-Salman-khan <alibinkhan465@gmail.com>
2026-02-06 14:35:58 +0500

    Add initial README, index.js, and observations.md files

---
+++ index.js
@@ -0,0 +1,10 @@
+// Task 1.1 – Losing and Fixing  this
+// Create an object with a method that uses  this .
+// Steps:
+// Call the method normally
+// Assign the method to a variable and call it
+// Fix the broken context using  call
+// Log outputs at each step and observe diferences.
+// Task 1.1 – Losing and Fixing  this (Start!)
+
+// Task 1.1 – Losing and Fixing  this (End!)
